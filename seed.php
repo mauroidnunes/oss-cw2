b10ce9794a2aed771bae940269d3906458d323ee
Mauro Nunes <21815118@bucks.ac.uk>
2022-05-15 20:20:17 +0100

    added 5 students

--- seed.php
+++ seed.php
@@ -4,12 +4,71 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
-    //checks whether we are logged in
+    //Checks if logged in
    if (isset($_SESSION['id'])) {
 
-    // build inster query
-    // run query
-    //x5
+    $array_students = array(
+        array(
+            "studentid" => "202201",
+            "password" => password_hash("password1", PASSWORD_DEFAULT),
+            "dob" => "03-05-1998",
+            "firstname" => "Jack",
+            "lastname" => "Walker",
+            "house" => "18 Jonsville Lane",
+            "town" => "Milton Keynes",
+            "county" => "Buckinghamshire",
+            "country" => "United Kingdom",
+            "postcode" => "MK9 5ER"
+        ),
+        array(
+            "studentid" => "202202",
+            "password" => password_hash("password2", PASSWORD_DEFAULT),
+            "dob" => "15-02-2002",
+            "firstname" => "Mary",
+            "lastname" => "Beckett",
+            "house" => "5 Rhode Square",
+            "town" => "Marlow",
+            "county" => "Buckinghamshire",
+            "country" => "United Kingdom",
+            "postcode" => "SL7 1AB"
+        ),
+        array(
+            "studentid" => "202203",
+            "password" => password_hash("password3", PASSWORD_DEFAULT),
+            "dob" => "26-08-2001",
+            "firstname" => "Maxime",
+            "lastname" => "Boulanger",
+            "house" => "4 London Road",
+            "town" => "High Wycombe",
+            "county" => "Buckinghamshire",
+            "country" => "United Kingdom",
+            "postcode" => "HP13 5DX"
+        ),
+        array(
+            "studentid" => "202204",
+            "password" => password_hash("password4", PASSWORD_DEFAULT),
+            "dob" => "04-07-2000",
+            "firstname" => "Filip",
+            "lastname" => "Radjenki",
+            "house" => "123 Hughenden Road",
+            "town" => "High Wycombe",
+            "county" => "Buckinghamshire",
+            "country" => "United Kingdom",
+            "postcode" => "HP11 3HE"
+        ),
+        array(
+            "studentid" => "202205",
+            "password" => password_hash("password5", PASSWORD_DEFAULT),
+            "dob" => "18-01-2000",
+            "firstname" => "Will",
+            "lastname" => "Barton",
+            "house" => "43 Tame House",
+            "town" => "Amersham",
+            "county" => "Buckinghamshire",
+            "country" => "United Kingdom",
+            "postcode" => "HP5 2ET"
+        ),
+    );
 
     for ($i=0; $i < 5 ; $i++) { 
         $sql = "INSERT INTO student ...";
